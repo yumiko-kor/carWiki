6605edcaa0cb5a4aec1ca5bbbb26706808a71c71
Yu <yugyeongjung@gmail.com>
2024-02-03 21:17:05 +0900

    feat: 버튼 컴포넌트 생성

--- src/styles/Component.js
+++ src/styles/Component.js
@@ -7,3 +7,22 @@ export const TextBox = styled.div`
     text-align: right;
     margin: ${props => props.$margin || 0};
 `;
+
+export const TextInput = styled.input`
+	border: 1px solid #e0e0e0;
+	border-radius: 15px;
+	width: ${props => props.width || '100%'};
+	padding: ${props => props.$padding || '5px 15px'};
+	line-height: 18px;
+	font-size: ${props => props.fontSize || '14px'};
+	&::placeholder {
+		color: #B1B1B1;
+	}
+	&:disabled {
+		color: #cccccc;
+		background: #f6f6f6;
+	}
+	&:focus {
+		border: 2px solid #72E6FF;
+	}
+`
