79eb092d7995bce043474f8ce360c34a1fa5e291
Yu <yugyeongjung@gmail.com>
2024-02-07 23:43:16 +0900

    feat: 구글 icon 변경 작업 완료

--- src/components/Login/LoginForGoogle.js
+++ src/components/Login/LoginForGoogle.js
@@ -35,21 +35,31 @@ const LoginForGoogle = () => {
             <GoogleOAuthProvider clientId={clientId}>
                 <LinkItem>
                     <Logo id="google-login-api" src={google} onClick={onClickGooglelogin} />
-                    <GoogleLogin
-                        onSuccess={credentialResponse => {
+                    <LoginWrap>
+                        <GoogleLogin
+                            clientId={clientId}
+                            onSuccess={credentialResponse => {
                             console.log(credentialResponse);
                             // 해당 부분은 후에 들어오는 토큰에 대해서 디코딩 하는 부분이다. 
                             // const decodeding = jwt_decode(credentialResponse.credential);
-                            console.log("success")
-                        }}
-                        onError={() => {
-                            console.log('Login Failed');
-                        }}
-                    />
+                                console.log("success")
+                            }}
+                            onError={() => {
+                                console.log('Login Failed');
+                            }}
+                            style={{ 
+                                display:"none"
+                            }}
+                        />  
+                    </LoginWrap>
                 </LinkItem>
             </GoogleOAuthProvider>
         </>
     );
 };
+
+const LoginWrap = styled.div`
+    display: none;
+`;
 
 export default LoginForGoogle;
